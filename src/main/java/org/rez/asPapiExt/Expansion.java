48897a50d55d21cfe58477d1b356d318a4128168
ETHG <skielias@aim.com>
2024-12-29 20:56:38 +0100

    Added functionality for a friend - Placeholders representing number of avaliable ghetto, upperclass, mansion, apartment, etc regions

--- src/main/java/org/rez/asPapiExt/Expansion.java
+++ src/main/java/org/rez/asPapiExt/Expansion.java
@@ -36,23 +36,17 @@ public class Expansion extends PlaceholderExpansion {
     public String onPlaceholderRequest(Player player, String identifier) {
         if (player == null) return null;
 
-        // Placeholder: %areashop_available_housing%
-        if (identifier.equals("available_housing")) {
-            return String.valueOf(areaShop.getFileManager().getRents().size());
-        }
 
         // Placeholder: %areashop_house_owned%
         if (identifier.equals("house_owned")) {
             for (int i = 0; i <areaShop.getFileManager().getRents().size(); i++) {
                 RentRegion thisRegion = areaShop.getFileManager().getRents().get(i);
                 if (thisRegion.isRenter(player)) {
-                    return "True";
+                    return thisRegion.getName();
                 }
             }
-            return "False";
+            return "No house owned";
         }
-
-
 
         // Placeholder: %areashop_house_owned_price%
         if (identifier.equals("house_owned_price")) {
@@ -65,29 +59,55 @@ public class Expansion extends PlaceholderExpansion {
             return "0";
         }
 
+        // Placeholder: %areashop_available_housing
+        if (identifier.equals("available_housing")) {
+            int count = 0;
+            for (int i = 0; i  <areaShop.getFileManager().getRents().size(); i++) {
+                RentRegion thisRegion = areaShop.getFileManager().getRents().get(i);
+                if (thisRegion.isRented()) {
+                    count++;
+                }
+            }
+
+            return String.valueOf(areaShop.getFileManager().getRents().size() - count);
+        }
 
         // Placeholder: %areashop_available_apartments%
-        //if (identifier.equals("available_apartments")) {}
+        // How many apartments are still available (integer)
+        if (identifier.equals("available_apartments")) {
+            return this.getNumOfAvailHousingOfType("apt");
+        }
 
         // Placeholder: %areashop_available_ghetto%
-        //if (identifier.equals("available_ghetto")) {
-        //}
+        if (identifier.equals("available_ghetto")) {
+            return this.getNumOfAvailHousingOfType("gh");
+        }
 
         // Placeholder: %areashop_available_upperclass%
-        //if (identifier.equals("available_upperclass")) {
-        //}
+        if (identifier.equals("available_upperclass")) {
+            return this.getNumOfAvailHousingOfType("up");
+        }
 
         // Placeholder: %areashop_available_mansions%
-        //if (identifier.equals("available_mansions")) {
-        //}
+        if (identifier.equals("available_mansions")) {
+            return this.getNumOfAvailHousingOfType("msn");
+        }
+
 
         return null; // If the placeholder is not recognized
     }
 
-//    private int getRegionsByType(String type) {
-//        // Custom method to filter regions by type
-//        return (int) areaShop.getFileManager().getRegions().stream()
-//                .filter(region -> region.getFlags().get("type").equals(type))
-//                .count();
-//    }
+    private String getNumOfAvailHousingOfType(String type) {
+        int count = 0;
+        for (int i = 0; i  <areaShop.getFileManager().getRents().size(); i++) {
+            RentRegion thisRegion = areaShop.getFileManager().getRents().get(i);
+            if (!thisRegion.isRented() && thisRegion.getName().contains(type)) {
+                count++;
+            }
+        }
+
+        return String.valueOf(count);
+
+    }
+
 }
